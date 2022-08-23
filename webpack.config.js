956c30642e9440b405f85117339d04f49ce973ed
Carlos Vivas <carlosmuerto@gmail.com>
2022-08-23 13:15:51 -0400

    Add Webpack files

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,61 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isDevelopment = process.env.NODE_ENV === 'development'
+const mode = process.argv[process.argv.indexOf('--mode') + 1];
+const devMode = mode !== 'production';
 
 module.exports = {
-	entry: {
-		index: './src/index.js',
-	},
-	mode: isDevelopment,
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-		}),
-	],
+  entry: {
+    index: './src/index.js',
+  },
+  mode,
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 
-	devtool: 'inline-source-map',
+  devtool: 'inline-source-map',
 
-	devServer: {
-		static: './dist',
-	},
+  devServer: {
+    static: './dist',
+  },
 
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/to-do-list-webpack/',
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/to-do-list-webpack/',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]',
+              },
+            },
+          },
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.scss'],
+  },
 
-	optimization: {
-		runtimeChunk: 'single',
-	},
-	performance: {
-		hints: true,
-	},
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  performance: {
+    // hints: false,
+  },
 };
